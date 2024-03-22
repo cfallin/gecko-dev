7a059c96d96421d8128b2d1400290b42b4b73c15
Chris Fallin <chris@cfallin.org>
2024-03-21 22:09:32 -0700

    PBL: in WASI build, do not count coverage.

--- js/src/vm/PortableBaselineInterpret.cpp
+++ js/src/vm/PortableBaselineInterpret.cpp
@@ -3359,6 +3359,7 @@ static EnvironmentObject& getEnvironmentFromCoordinate(
 #  define PREDICT_NEXT(op)
 #endif
 
+#ifndef __wasi__
 #define COUNT_COVERAGE_PC(PC)                        \
   if (script->hasScriptCounts()) {                   \
     PCCounts* counts = script->maybeGetPCCounts(PC); \
@@ -3370,6 +3371,10 @@ static EnvironmentObject& getEnvironmentFromCoordinate(
     jsbytecode* main = script->main();                               \
     if (!BytecodeIsJumpTarget(JSOp(*main))) COUNT_COVERAGE_PC(main); \
   }
+#else
+#define COUNT_COVERAGE_PC(PC) ;
+#define COUNT_COVERAGE_MAIN() ;
+#endif
 
 #define NEXT_IC() frame->interpreterICEntry()++;
 
